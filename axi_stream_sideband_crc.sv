752f5fe478122481bf7b9151475e154200ed2e75
archy <blossembeee@gmail.com>
2022-03-18 19:00:30 +0500

    ctreate vivado project, add init hdl file

---
+++ axi_stream_sideband_crc.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/18/2022 06:10:44 PM
+// Design Name: 
+// Module Name: axi_stream_sideband_crc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module axi_stream_sideband_crc(
+
+    );
+endmodule
