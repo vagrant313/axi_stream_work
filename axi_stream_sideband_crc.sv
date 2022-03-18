aa1547e61879b334a5d790c876ddd33c0ec4787a
archy <blossembeee@gmail.com>
2022-03-18 19:27:54 +0500

    change axi stream signals more readable

--- axi_stream_sideband_crc.sv
+++ axi_stream_sideband_crc.sv
@@ -23,26 +23,28 @@
 module axi_stream_sideband_crc #(
 
     parameter DATA_WIDTH = 512,
-    parameter KEEP_BYTES = DATA_WIDTH/8 )
+    parameter KEEP_BYTES = DATA_WIDTH/8,
+    parameter CRC_WIDTH  = 32 )
 (
 
-        Input              clk, 
-        Input              srst, 
+        input                         clk, 
+        input                         srst, 
         
         // AXI-ST signals from source 
-        Input              i_tlast, 
-        Input 	   [511:0] i_tdata, 
-        Input  [512/8-1:0] i_tkeep, 
-        Input              i_tvalid, 
-        Input              o_tready, 
-        Input  [     31:0] crc, 
+        input                         i_s_tlast, 
+        input 	   [DATA_WIDTH-1:0]   i_s_tdata, 
+        input      [DATA_WIDTH/8-1:0] i_s_tkeep, 
+        input                         i_s_tvalid, 
+        output                        o_s_tready, 
+
+        input      [CRC_WIDTH:0]      crc, 
         
         // AXI-ST signals to sink 
-        Input             o_tlast, 
-        Input     [511:0] o_tdata, 
-        Input [512/8-1:0] o_tkeep, 
-        Input             o_tvalid, 
-        Input             i_tready 
+        input                         o_m_tlast, 
+        input      [DATA_WIDTH:0]     o_m_tdata, 
+        input      [DATA_WIDTH/8-1:0] o_m_tkeep, 
+        input                         o_m_tvalid, 
+        input                         i_m_tready 
     
     );
 endmodule
