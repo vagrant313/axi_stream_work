6a4aa22d978dbd2d95730553daf4a95c91d6ed9b
archy <blossembeee@gmail.com>
2022-03-27 12:44:51 +0500

    all the outputs are working as assigned

--- axi_stream_sideband_crc.sv
+++ axi_stream_sideband_crc.sv
@@ -1,83 +1,247 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/18/2022 06:10:44 PM
-// Design Name: 
-// Module Name: axi_stream_sideband_crc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module axi_stream_sideband_crc #(
+module axi_stream_sideband_crc  #(
 
     parameter DATA_WIDTH = 512,
     parameter KEEP_BYTES = DATA_WIDTH/8,
     parameter CRC_WIDTH  = 32 )
-(
+  (
 
-        input                            clk, 
-        input                            srst, 
-        
-        // AXI-ST signals from source 
-        input   wire                      i_s_tlast, 
-        input 	wire   [DATA_WIDTH-1:0]   i_s_tdata, 
-        input   wire   [DATA_WIDTH/8-1:0] i_s_tkeep, 
-        input   wire                      i_s_tvalid, 
-        output  reg                       o_s_tready, 
+    input                        clk,
+    input                        srst,
 
-        input   wire   [CRC_WIDTH:0]      crc, 
-        
-        // AXI-ST signals to sink 
-        output   reg                      o_m_tlast, 
-        output   reg   [DATA_WIDTH:0]     o_m_tdata, 
-        output   reg   [DATA_WIDTH/8-1:0] o_m_tkeep, 
-        output   reg                      o_m_tvalid, 
-        input    wire                     i_m_tready 
-    
-    );
+    // AXI-ST signals from source
+    input   wire                      i_s_tlast,
+    input 	wire   [DATA_WIDTH-1:0]   i_s_tdata,
+    input   wire   [DATA_WIDTH/8-1:0] i_s_tkeep,
+    input   wire                      i_s_tvalid,
+    output  reg                       o_s_tready,
 
-    reg [15:0]bytes_in_word_now;
-    reg [15:0]bytes_in_word_nxt;
+    input   wire   [CRC_WIDTH-1:0]      crc,
 
-    integer i, byte_count;
+    // AXI-ST signals to sink
+    output   reg                      o_m_tlast,
+    output   reg   [DATA_WIDTH-1:0]   o_m_tdata,
+    output   reg   [DATA_WIDTH/8-1:0] o_m_tkeep,
+    output   reg                      o_m_tvalid,
+    input    wire                     i_m_tready
+
+  );
 
 
-    always@* begin
 
-        bytes_in_word_nxt =  bytes_in_word_now;
-               
-        if (i_s_tvalid) begin  //&& o_s_tready
- 
-                byte_count = 0;
-                for (i=0; i <= KEEP_BYTES; i++) begin
-                    if(i_s_tkeep[i])
-                        byte_count = byte_count + 1;
-                end
-                
-                bytes_in_word_nxt = bytes_in_word_nxt + byte_count;
+
+
+  logic                      o_m_tlast_reg;
+  logic   [DATA_WIDTH-1:0]   o_m_tdata_reg;
+  logic   [DATA_WIDTH/8-1:0] o_m_tkeep_reg;
+  logic                      o_m_tvalid_reg;
+  logic                      o_s_tready_reg;
+
+  logic   [DATA_WIDTH-1:0]   crc_long_reg    = '0;
+  logic   [KEEP_BYTES-1:0]   keep_long_reg   = '0;
+  int     crc_overflow = -1;
+
+
+
+
+  enum { IDLE, // wait for tvalid
+         READ, // passthrough input words
+         EXTD // extend crc to another word with backpressue
+       } state,next;
+
+
+
+  always @(posedge clk)
+  begin
+    if(srst)  state <= IDLE;
+    else      state <= next;
+  end
+
+
+
+  always @(*)
+  begin
+
+    next = state;
+
+    case(state)
+
+      IDLE:
+      begin
+
+
+        o_m_tlast_reg  = '0;
+        o_m_tdata_reg  = '0;
+        o_m_tkeep_reg  = '0;
+        o_m_tvalid_reg = '0;
+        o_s_tready_reg = '1;
+
+        if(i_s_tvalid && o_s_tready)
+        begin
+
+          o_m_tlast_reg  = '0;
+          o_m_tdata_reg  = i_s_tdata;
+          o_m_tkeep_reg  = i_s_tkeep;
+          o_m_tvalid_reg = '1;
+          o_s_tready_reg = '1;
+
+          next = READ;
         end
-    
-    end
-    
-    
-    always@(posedge clk or posedge srst) begin
-        if(srst)
-            bytes_in_word_now <= 0;
         else
-            bytes_in_word_now <= bytes_in_word_nxt;
-    
+        begin
+          next = IDLE;
+        end
+      end
+
+      READ:
+      begin
+
+
+        if(!(i_s_tlast))
+        begin
+
+          o_m_tlast_reg  = '0;
+          o_m_tvalid_reg = '1;
+          o_s_tready_reg = '1;
+
+          o_m_tdata_reg = i_s_tdata;
+          o_m_tkeep_reg = i_s_tkeep;
+
+          next = READ;
+        end
+
+        else if(i_s_tlast && crc_overflow >= 4 )
+        begin
+
+          o_m_tlast_reg  = '1;
+          o_m_tvalid_reg = '1;
+          o_s_tready_reg = '1;
+
+          o_m_tdata_reg = crc_long_reg  | i_s_tdata;
+          o_m_tkeep_reg = keep_long_reg | i_s_tkeep;
+
+          next = IDLE;
+        end
+        else
+        begin
+
+          o_m_tlast_reg  = '0;
+          o_m_tvalid_reg = '1;
+          o_s_tready_reg = '1;
+
+          o_m_tdata_reg = crc_long_reg  | i_s_tdata;
+          o_m_tkeep_reg = '1;
+
+          next = EXTD;
+        end
+      end
+
+      EXTD:
+      begin
+
+        if(i_s_tlast && crc_overflow < 4 )
+        begin
+          o_m_tlast_reg  = '1;
+          o_m_tvalid_reg = '1;
+          o_s_tready_reg = '0;
+
+          o_m_tdata_reg = crc     >>  (crc_overflow*8);
+          o_m_tkeep_reg = 4'b1111 >>  (crc_overflow);
+
+
+          next = IDLE;
+        end
+        else
+        begin
+          next = IDLE;
+        end
+      end
+
+      default:
+        next = IDLE;
+
+    endcase
+
+  end
+
+
+  // synchronous Output connections to avoid glitsches
+
+  always @(posedge clk )
+  begin
+    if(srst)
+    begin
+      o_m_tlast  <= '0;
+      o_m_tvalid <= '0;
+      o_m_tdata  <= '0;
+      o_m_tkeep  <= '0;
+      o_s_tready <= '0;
     end
+    else if(i_m_tready)
+    begin
+
+      o_m_tlast   <= o_m_tlast_reg;
+      o_m_tvalid  <= o_m_tvalid_reg;
+      o_m_tdata   <= o_m_tdata_reg;
+      o_m_tkeep   <= o_m_tkeep_reg;
+      o_s_tready  <= o_s_tready_reg;
+
+    end
+    else
+    begin
+      o_m_tlast  <= '0;
+      o_m_tvalid <= '0;
+      o_m_tdata  <= '0;
+      o_m_tkeep  <= '0;
+      o_s_tready <= '0;
+    end
+  end
+
+
+
+  ///////////////////////////////////////////////
+  //                                           //
+  //  Tracking logic for last word bytes count //
+  //                                           //
+  ///////////////////////////////////////////////
+
+  int keep_valid_index;
+
+  always@*
+  begin
+
+    keep_valid_index = 0;
+    crc_long_reg    = crc;
+    keep_long_reg   = {{(KEEP_BYTES-4){1'b0}}, 4'b1111};
+
+    if (i_s_tlast)
+    begin
+
+      for (keep_valid_index=0; keep_valid_index < KEEP_BYTES; keep_valid_index++)
+      begin
+        if(i_s_tkeep[keep_valid_index])
+        begin
+
+          crc_long_reg  = crc_long_reg << 8;
+
+          crc_overflow  = (KEEP_BYTES-keep_valid_index-1);
+
+          keep_long_reg =  keep_long_reg << 1;
+
+
+        end
+      end
+    end
+
+  end
+
 
 endmodule
--- tb_axi_stream_sideband_crc.sv
+++ tb_axi_stream_sideband_crc.sv
@@ -1,18 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/18/2022 09:09:37 PM
-// Design Name: 
-// Module Name: tb_axi_stream_sideband_crc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
@@ -20,7 +7,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb_axi_stream_sideband_crc(
+module tb_axi_stream_sideband_crc (
 
     );
     
@@ -33,22 +20,23 @@ module tb_axi_stream_sideband_crc(
     
     // AXI-ST signals from source 
     reg                      i_s_tlast; 
+    reg                      i_s_tlast1; 
     reg   [DATA_WIDTH-1:0]   i_s_tdata; 
     reg   [DATA_WIDTH/8-1:0] i_s_tkeep; 
     reg                      i_s_tvalid; 
     wire                     o_s_tready; 
 
-    reg   [CRC_WIDTH:0]      crc; 
+    reg   [CRC_WIDTH-1:0]      crc; 
     
     // AXI-ST signals to sink 
     wire                      o_m_tlast; 
-    wire   [DATA_WIDTH:0]     o_m_tdata; 
+    wire   [DATA_WIDTH-1:0]   o_m_tdata; 
     wire   [DATA_WIDTH/8-1:0] o_m_tkeep; 
     wire                      o_m_tvalid; 
     reg                       i_m_tready; 
     
     
-    axi_stream_sideband_crc DUT(
+    axi_stream_sideband_crc  DUT(
         .clk(clk),
         .srst(srst),
         .i_s_tlast(i_s_tlast),
@@ -64,32 +52,157 @@ module tb_axi_stream_sideband_crc(
         .i_m_tready(i_m_tready)
     );
     
+    parameter PERIOD = 20;
+    
     initial begin
-        clk = 1;
+        #(PERIOD/2)  clk = 0;
         forever begin
-         #5 clk = ~clk;
+         #(PERIOD/2) clk = ~clk;
         end
     end
     
-    initial begin
-        i_s_tlast  = 0;
-        i_s_tdata  = {DATA_WIDTH{1'b1}};
-        i_s_tkeep  = 0;
-        i_s_tvalid = 0;
-        //o_s_tready = 1;
-        
-        #00;  srst = 0;
-        #10;  srst = 1;
-        #10;  srst = 0;
-        
-        #10; i_s_tvalid = 1; i_s_tkeep  = {KEEP_BYTES{1'b1}}; 
-        #40; i_s_tvalid = 0; i_s_tkeep  = 0;
-    end
+    
     
     initial begin
-        # 60; i_s_tlast  = 1; i_s_tkeep  = {(KEEP_BYTES-62){1'b1}};
-        # 10; i_s_tlast  = 0;
+        i_s_tlast  = 0;
+        i_s_tdata  = {{(DATA_WIDTH-64){1'b1}}, 64'hDEAD_BEEF_DEAD_BEEF};
+        i_s_tkeep  = 0;
+        i_s_tvalid = 0;
+        i_m_tready = 1;
+        #2
+                
+        // reset 
+        #00;  srst = 0;
+        #(PERIOD*2);  
+        srst = 1;
+        #(PERIOD*4);
+        srst = 0;
+        
+
+        //////////////////////////////////////////////////////////
+        //
+        //create 4 words packet to test first CASE
+        //
+        //////////////////////////////////////////////////////////
+        #00; crc        = 32'hCECDCBCA;
+        
+        #(PERIOD*4) 
+        i_s_tvalid = 1; 
+        i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+        i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+
+        #(PERIOD*1)
+        i_s_tvalid = 1;
+        i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+        i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b0}}};
+
+        #(PERIOD*1) 
+        i_s_tvalid = 1;
+        i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+        i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+
+        #(PERIOD*1) 
+        i_s_tvalid = 1;
+        i_s_tkeep  = {8'b0000_0011, {(KEEP_BYTES-8){1'b1}} };
+        i_s_tdata  = {64'h0000_0000_0000_BEEF,{(DATA_WIDTH-64){1'b1}}};
+        i_s_tlast  = 1; 
+
+        #(PERIOD*1); i_s_tvalid = 0; i_s_tkeep  = 0; i_s_tlast  = 0; i_s_tkeep  = {(KEEP_BYTES){1'b0}};
+
+
+        ////////////////////////////////////////////////////////
+        //
+        //4 words packet, test 2nd CASE with last 4 bytes empty
+        //
+        ////////////////////////////////////////////////////////
+        #00; crc        = 32'hABCDEF45;
+ 
+       #(PERIOD*4) 
+         i_s_tvalid = 1; 
+         i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+         i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+ 
+         #(PERIOD*1)
+         i_s_tvalid = 1;
+         i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+         i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b0}}};
+ 
+         #(PERIOD*1) 
+         i_s_tvalid = 1;
+         i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+         i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+ 
+         #(PERIOD*1) 
+         i_s_tvalid = 1;
+         i_s_tkeep  = {8'b0000_1111, {(KEEP_BYTES-8){1'b1}} };
+         i_s_tdata  = {64'h0000_0000_0000_BEEF,{(DATA_WIDTH-64){1'b1}}};
+         i_s_tlast  = 1; 
+ 
+         #(PERIOD*1); i_s_tvalid = 0; i_s_tkeep  = 0; i_s_tlast  = 0; i_s_tkeep  = {(KEEP_BYTES){1'b0}};
+
+
+        //////////////////////////////////////////////////////////
+        //
+        //4 words packet, test 2nd CASE with only last byte empty
+        //
+        //////////////////////////////////////////////////////////
+        #00; crc        = 32'h12CDEF23;
+
+       #(PERIOD*4) 
+        i_s_tvalid = 1; 
+        i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+        i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+
+        #(PERIOD*1)
+        i_s_tvalid = 1;
+        i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+        i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b0}}};
+
+        #(PERIOD*1) 
+        i_s_tvalid = 1;
+        i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+        i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+
+        #(PERIOD*1) 
+        i_s_tvalid = 1;
+        i_s_tkeep  = {8'b0111_1111, {(KEEP_BYTES-8){1'b1}} };
+        i_s_tdata  = {64'h0000_0000_0000_BEEF,{(DATA_WIDTH-64){1'b1}}};
+        i_s_tlast  = 1; 
+
+        #(PERIOD*1); i_s_tvalid = 0; i_s_tkeep  = 0; i_s_tlast  = 0; i_s_tkeep  = {(KEEP_BYTES){1'b0}};
+
+        ////////////////////////////////////////////////////////////
+        //
+        //4 words packet, test 2nd CASE with all last bytes filled
+        //
+        ////////////////////////////////////////////////////////////
+        #00; crc        = 32'h34CDEF11;
+
+       #(PERIOD*4) 
+         i_s_tvalid = 1; 
+         i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+         i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+ 
+         #(PERIOD*1)
+         i_s_tvalid = 1;
+         i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+         i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b0}}};
+ 
+         #(PERIOD*1) 
+         i_s_tvalid = 1;
+         i_s_tkeep  = {(KEEP_BYTES){1'b1}};
+         i_s_tdata  = {64'hDEAD_BEEF_DEAD_BEEF,{(DATA_WIDTH-64){1'b1}}};
+ 
+         #(PERIOD*1) 
+         i_s_tvalid = 1;
+         i_s_tkeep  = {8'b1111_1111, {(KEEP_BYTES-8){1'b1}} };
+         i_s_tdata  = {64'h0000_0000_0000_BEEF,{(DATA_WIDTH-64){1'b1}}};
+         i_s_tlast  = 1; 
+ 
+         #(PERIOD*1); i_s_tvalid = 0; i_s_tkeep  = 0; i_s_tlast  = 0; i_s_tkeep  = {(KEEP_BYTES){1'b0}};
+        
 
     end
+    
         
 endmodule
